9b7fd16110b150bf22256c52240038be56804b3e
mbway <matthewbroadway@msn.com>
2018-06-09 13:25:23 +0100

    accurate mouse aiming

--- Weapons/Pistol.gd
+++ Weapons/Pistol.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-onready var bullet_scene = load('res://Bullet.tscn')
+onready var bullet_scene = load('res://Weapons/Bullet.tscn')
 
 export (int) var shoot_vel = 2000
+
 
 func _process(delta):
 	if Input.is_action_just_pressed('fire'):
