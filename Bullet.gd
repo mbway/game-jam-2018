1c13cd4738f2a829e94defffed8443cd6525ca19
mbway <matthewbroadway@msn.com>
2018-06-09 01:08:35 +0100

    pistol can shoot

--- Bullet.gd
+++ Bullet.gd
@@ -2,29 +2,45 @@ extends RigidBody2D
 
 const MAX_DISTANCE = 1000
 
-var travelled = 0
-var shot_from = None
-var spawn_loc = None
-var parent = None
+var shot_from
+var vel
+
+var spawn_loc
+var shoot_direction
 
 func _ready():
 	pass
 
-func setup(shot_from):
-	.shot_from = shot_from
-	spawn_loc = global_position
-	parent = get_node('/root/Bullets')
+func setup(shot_from, vel):
+	self.shot_from = shot_from
+	self.vel = vel
+	
+	# can't use self.get_node because may not be ready/added to the tree yet
+	var parent = shot_from.get_node('/root/Main/Bullets')
 	parent.add_child(self)
-	position = shot_from.get_node('Muzzle').position
-	set_z(0)
-	connect('body_entered', self, 'kill')
+	
+	position = shot_from.get_node('Muzzle').global_position
+	spawn_loc = global_position
+	shoot_direction = Vector2(1, 0).rotated(shot_from.rotation)
+	#set_z_index(0)
+	
+	#print('bullet setup')
 
-# _fixed_process is synced to physics
-func _fixed_process(delta):
-	travelled += global_position.distance_to(spawn_loc)
-	if travelled > MAX_DISTANCE:
+
+# before godot 3, used to be called _fixed_process
+func _physics_process(delta):
+	if global_position.distance_to(spawn_loc) > MAX_DISTANCE:
 		kill()
+	
+	position += shoot_direction * vel * delta
 
 
 func kill():
+	print('deleted')
+	hide()
 	queue_free()
+
+
+func _on_Bullet_body_entered(body):
+	print(body)
+	kill()
