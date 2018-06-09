e70331cd2d08f1bf1b1f49ce819a5cdce350b4e0
mbway <matthewbroadway@msn.com>
2018-06-09 15:47:56 +0100

    more dynamic input

--- Player.gd
+++ Player.gd
@@ -6,12 +6,15 @@ extends KinematicBody2D
 ## CONSTANTS ##
 const UP = Vector2(0, -1)
 const BEFORE_START = -999 # timestamp long before the start of the game (ms)
+const JOY_X = 2
+const JOY_Y = 3
+const JOY_DEADZONE = 0.2
 
 # set when spawned in
 var is_setup = false
-var camera = null # for user input purposes
+var input_prefix = null
+var camera = null # used for mouse input, null => 
 var bullet_parent = null # the node to spawn the bullets under
-var fire_action = null # the input map name which fires the player's gun
 
 
 
@@ -40,9 +43,9 @@ const MAX_ADDITIONAL_JUMPS = 1
 var additional_jumps_left = 1 # to allow double jumps, reset when touching the floor
 
 
-func setup(bullet_parent, fire_action, camera):
+func setup(input_prefix, bullet_parent, camera):
+	self.input_prefix = input_prefix
 	self.bullet_parent = bullet_parent
-	self.fire_action = fire_action
 	self.camera = camera
 	is_setup = true
 
@@ -52,13 +55,25 @@ func _process(delta):
 		
 	if has_node('Gun'):
 		# gun rotation
-		# maths copied from power defence
-		var mouse_pos = camera.get_local_mouse_position()
-		var gun_pos = $Gun.get_position()
-		var angle = mouse_pos.angle_to_point(gun_pos)
-		var d = (mouse_pos - gun_pos).length()
-		var o = ($Gun/Muzzle.get_position()-gun_pos).y
-		var angle_correction = asin(o/d)
+		var angle = 0
+		var angle_correction = 0
+		
+		if camera == null:
+			# gamepad
+			var direction = Vector2(Input.get_joy_axis(0, JOY_X), Input.get_joy_axis(0, JOY_Y))
+			if direction.length() > JOY_DEADZONE:
+				angle = direction.angle()
+			else:
+				angle = $Gun.rotation
+		else:
+			# mouse
+			# maths copied from power defence
+			var gun_pos = $Gun.get_position()
+			var mouse_pos = camera.get_local_mouse_position()
+			angle = mouse_pos.angle_to_point(gun_pos)
+			var d = (mouse_pos - gun_pos).length()
+			var o = ($Gun/Muzzle.get_position()-gun_pos).y
+			angle_correction = asin(o/d)
 
 		if abs(angle) > PI/2:
 			$Gun.scale.y = -1
@@ -77,9 +92,9 @@ func _physics_process(delta):
 
 	# get the direction from user input
 	var direction = 0
-	if Input.is_action_pressed('ui_left'):
+	if Input.is_action_pressed(input_prefix + 'left'):
 		direction += -1
-	if Input.is_action_pressed('ui_right'):
+	if Input.is_action_pressed(input_prefix + 'right'):
 		direction += 1
 
 	# calculate the speed
@@ -93,7 +108,7 @@ func _physics_process(delta):
 	var on_floor = is_on_floor()
 
 	# able to register a jump if character just about to hit the floor
-	if Input.is_action_just_pressed('ui_up'):
+	if Input.is_action_just_pressed(input_prefix + 'up'):
 		last_jump_ms = now
 
 	if now - last_jump_ms < PREEMPTIVE_JUMP_TOLERANCE: # want to jump
@@ -120,7 +135,7 @@ func _physics_process(delta):
 			left_floor_ms = now
 
 		# cancel jump in mid air
-		if Input.is_action_just_released('ui_up'):
+		if Input.is_action_just_released(input_prefix + 'up'):
 			# if still moving up, then stop moving up
 			velocity.y = max(velocity.y, -JUMP_SPEED/5)
 
@@ -149,7 +164,7 @@ func equip(gun_scene):
 	assert not has_node('Gun')
 	var gun = gun_scene.instance()
 	gun.set_name('Gun')
-	gun.setup(bullet_parent, fire_action)
+	gun.setup(bullet_parent, input_prefix + 'fire')
 	add_child(gun)
 
 
