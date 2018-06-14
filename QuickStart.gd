47046442d08d558ece43d5bc334e70cc3d292fdb
mbway <matthewbroadway@msn.com>
2018-06-14 17:34:41 +0100

    fixed quick start

--- QuickStart.gd
+++ QuickStart.gd
@@ -8,8 +8,12 @@ func _ready():
 	globals = get_node('/root/globals')
 	
 	globals.player_data = [
-		globals.PlayerData.new(1, 'Player 1', 1, globals.KEYBOARD_CONTROL),
-		globals.PlayerData.new(2, 'Player 2', 2, globals.GAMEPAD_CONTROL, 0),
-		globals.PlayerData.new(2, 'Player 3', 2, globals.AI_CONTROL)
+		globals.PlayerConfig.new(1, 'Player 1', 1, globals.KEYBOARD_CONTROL),
+		globals.PlayerConfig.new(2, 'Player 2', 2, globals.GAMEPAD_CONTROL, 0),
+		#globals.PlayerConfig.new(2, 'Player 3', 2, globals.AI_CONTROL)
 	]
-	
+	globals.game_mode = 'TDM'
+	globals.game_mode_details = {
+		'max_lives' : 1
+	}
+	get_tree().change_scene('res://Maps/UFO.tscn')
