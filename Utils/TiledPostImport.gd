b02bce44d4c033164213a05b64f755c49e36bb81
Matthew Broadway <mattdbway@gmail.com>
2020-01-11 13:11:35 +0000

    fixed issue with tiled imports being offset incorrectly

--- Utils/TiledPostImport.gd
+++ Utils/TiledPostImport.gd
@@ -14,14 +14,20 @@ func post_import(scene):
 
 	var collision = scene.get_node('Collision')
 	for c in collision.get_children():
-		c.collision_layer = 2
-		c.collision_mask = 0
+		_init_collision(c)
 
 	if scene.has_node('OneWayCollision'): # optional
 		var one_way_collision = scene.get_node('OneWayCollision')
 		for c in one_way_collision.get_children():
-			c.collision_layer = 2
-			c.collision_mask = 0
+			_init_collision(c)
 			c.get_node('CollisionShape2D').one_way_collision = true
 
 	return scene
+
+static func _init_collision(c):
+	c.collision_layer = 2
+	c.collision_mask = 0
+
+	# required due to the following bug. Remove once fixed upstream
+	# https://github.com/vnen/godot-tiled-importer/issues/114
+	c.position.y -= 32
